e8b006ca13bc06463ba7b1972f15ad258c9f623d
AUnicornWithNoLife <benjamin.barratt@icloud.com>
2021-04-13 22:18:47 +0100

    themes

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+class theme
+{
+    constructor(back, object, highlight, text)
+    {
+        this.back = back;
+        this.object = object;
+        this.highlight = highlight;
+        this.text = text;
+    }
+}
+
+function setTheme(name)
+{
+
+}
+
+const themes = 
+{
+    'orange': theme('#231F20', '#ec5020', '#fa6d42', '#ffffff')
+}
